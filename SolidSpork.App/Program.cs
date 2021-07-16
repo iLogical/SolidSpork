f9f4706b8d457c753dd5ff9f6a6c6aa1199afe36
iLogical <iLogical8502@gmail.com>
2021-07-16 21:45:16 +0100

    Initial Commit

---
+++ SolidSpork.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidSpork.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
